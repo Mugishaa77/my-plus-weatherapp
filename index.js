03c329b2c3f4607a19514b3fe786bc86fcbdd8bb
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2022-12-11 10:58:01 +0300

    Replaced the incomplete package.json file with a better one

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-let now = new Date();
-console.log(now);
+let now = new Date ();
 
 let days = [
   "Sunday",
@@ -13,40 +12,25 @@ let days = [
 ];
 
 let currentDay = days[now.getDay()];
-console.log(currentDay);
+
 
 let presentDay = document.querySelector(".day");
 presentDay.innerHTML = currentDay;
 
 let hours = now.getHours();
-console.log(hours);
 
 let minutes = now.getMinutes();
-console.log(minutes);
+
 
 let currentTime = document.querySelector(".date");
 currentTime.innerHTML = hours + ":" + minutes;
 
-function searchButton(event) {
+function search(event) {
   event.preventDefault();
-  let searchResult = document.querySelector("#field");
-
-  let city = document.querySelector("#ccity");
-  city.innerHTML = searchResult.value;
-}
-
-let form = document.querySelector("#searching");
-form.addEventListener("submit", searchButton);
-
-function showTemperature(response) {
-  let searchResult = response.main.data.temperature;
-  console.log(searchResult);
-
-  let currentTemp = document.querySelector(".class");
-  currentTemp.innerHTML = searchResult;
   let apiKey = "9219390dd657b91d9e5236012585e598";
-  let cityName = document.querySelector("#field.value");
-  let apiUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&unit=metric";
+  let cityName = "Nairobi";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
 }
-axios.get(apiUrl).then(showTemperature);
+
+  
